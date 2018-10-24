9e29c84b4340f973340582d16feb6bce72455f1d
Espen Olsen <eo@friendup.cloud>
2018-10-24 16:18:59 +0200

    asd

--- client/update.sh
+++ client/update.sh
@@ -5,33 +5,7 @@
 # the proper location in Friend build directory structure.
 
 # Edit this line if your Friend directory is not the same as this one
-FRIEND="/home/$USER/friendup"
-
-# Eventually asks for the good directory
-if [ ! -f "$FRIEND/build/cfg/cfg.ini" ]; then
-    while true; do
-        temp=$(dialog --backtitle "Friend Chat client update" --inputbox "\
-Please enter the path to the FriendUP directory." 11 60 "$FRIEND" --output-fd 1)
-        if [ $? = "1" ]; then
-            clear
-            echo "Update aborted."
-            exit 1
-        fi
-        if [ $temp != "" ]; then
-            FRIEND="$temp"
-        fi
-
-        # Verifies the directory
-        if [ ! -f "$FRIEND/build/cfg/cfg.ini" ]; then
-            dialog --backtitle "Friend Chat client update" --msgbox "\
-Friend was not found in this directory,\n\
-or Friend was not properly installed." 10 50
-        else
-            clear
-            break;
-        fi
-    done
-fi
+echo "updating client, path: $FRIEND"
 
 # Creates destination directory if it does not exist
 if [ ! -d "$FRIEND/build/resources/webclient/apps/FriendChat" ]; then
